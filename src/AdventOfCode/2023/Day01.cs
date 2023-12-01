650e4b5bcaed1405827f07f1f70ef5300a8bfadd
Robin Rexstedt <robin.aakerman@gmail.com>
2023-12-01 06:38:44 +0100

    Day 01 first attempt

--- src/AdventOfCode/2023/Day01.cs
+++ src/AdventOfCode/2023/Day01.cs
@@ -4,18 +4,80 @@ namespace AdventOfCode._2023;
 
 public sealed class Day01 : AdventBase
 {
-    protected override void InternalOnLoad()
-    {
-        base.InternalOnLoad();
-    }
-
     protected override object InternalPart1()
     {
-        throw new NotImplementedException();
+        return Input.Lines.Select(CombineFirstAndLastDigit).Sum();
+    }
+
+    private int CombineFirstAndLastDigit(string source)
+    {
+        var first = source.First(char.IsDigit);
+        var last = source.Last(char.IsDigit);
+
+        return int.Parse($"{first}{last}");
     }
 
     protected override object InternalPart2()
     {
-        throw new NotImplementedException();
+        return Input.Lines.Select(CombineFirstAndLastDigitP2).Sum();
+    }
+
+    private Dictionary<string, int> _numberLookup = new()
+    {
+        ["one"] = 1,
+        ["two"] = 2,
+        ["three"] = 3,
+        ["four"] = 4,
+        ["five"] = 5,
+        ["six"] = 6,
+        ["seven"] = 7,
+        ["eight"] = 8,
+        ["nine"] = 9,
+        ["1"] = 1,
+        ["2"] = 2,
+        ["3"] = 3,
+        ["4"] = 4,
+        ["5"] = 5,
+        ["6"] = 6,
+        ["7"] = 7,
+        ["8"] = 8,
+        ["9"] = 9,
+    };
+
+    private int CombineFirstAndLastDigitP2(string source)
+    {
+        var bestIndex = source.Length + 1;
+        var bestValue = "";
+
+        foreach (KeyValuePair<string, int> pair in _numberLookup)
+        {
+            var index = source.IndexOf(pair.Key, StringComparison.InvariantCulture);
+
+            if (index != -1 && index < bestIndex)
+            {
+                bestIndex = index;
+                bestValue = pair.Value.ToString();
+            }
+        }
+
+        var firstDigit = bestValue;
+        bestIndex = int.MinValue;
+        bestValue = "";
+
+        foreach (KeyValuePair<string, int> pair in _numberLookup)
+        {
+            var index = source.LastIndexOf(pair.Key, StringComparison.InvariantCulture);
+
+            if (index != -1 && index > bestIndex)
+            {
+                bestIndex = index;
+                bestValue = pair.Value.ToString();
+            }
+        }
+
+        var secondDigit = bestValue;
+
+
+        return CombineFirstAndLastDigit($"{firstDigit}{secondDigit}");
     }
 }
--- tests/AdventTests/2023/Day01Tests.cs
+++ tests/AdventTests/2023/Day01Tests.cs
@@ -1,6 +1,43 @@
-﻿namespace AdventTests2023._2023;
+﻿using AdventOfCode._2023;
 
-public sealed class Day01Tests
+namespace AdventTests._2023;
+
+public class Day01Tests
 {
-    
+    private string _testInput = """
+                                1abc2
+                                pqr3stu8vwx
+                                a1b2c3d4e5f
+                                treb7uchet
+                                """;
+
+    private string _part2TestInput = """
+                                     two1nine
+                                     eightwothree
+                                     abcone2threexyz
+                                     xtwone3four
+                                     4nineeightseven2
+                                     zoneight234
+                                     7pqrstsixteen
+                                     """;
+
+    [Fact]
+    public void Part1()
+    {
+        var day = new Day01();
+
+        day.SetTestInput(_testInput);
+        day.Part1();
+        day.Part1Answer.ShouldBe("142");
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        var day = new Day01();
+
+        day.SetTestInput(_part2TestInput);
+        day.Part2();
+        day.Part2Answer.ShouldBe("281");
+    }
 }
