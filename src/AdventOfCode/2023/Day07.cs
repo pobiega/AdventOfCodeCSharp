bca5a3f9b43c8685048ba4b7fa433c313e5ec13d
Robin Rexstedt <robin.aakerman@gmail.com>
2023-12-09 23:28:26 +0100

    Add day 7, broken part 2

--- src/AdventOfCode/2023/Day07.cs
+++ src/AdventOfCode/2023/Day07.cs
@@ -1,6 +1,231 @@
-﻿namespace AdventOfCode._2023;
+﻿using AdventOfCodeSupport;
 
-public sealed class Day07
+namespace AdventOfCode._2023;
+
+public sealed class Day07 : AdventBase
 {
-    
+    protected override object InternalPart1()
+    {
+        var hands = Parse();
+        hands.Sort(_part1Comparer);
+
+        var score = hands
+            .Select((hand, i) => hand.Bid * (i + 1))
+            .Sum();
+
+        return score;
+    }
+
+    private List<CamelHand> Parse()
+    {
+        return Input.Lines.Select(ParseOne).ToList();
+    }
+
+    public static CamelHand ParseOne(string line)
+    {
+        var cards = line.Split(' ')[0].Select(CharToCard).ToArray();
+
+        var bid = line.Split(' ')[1].ToInt32();
+
+        if (cards.All(x => x == cards[0]))
+        {
+            return new CamelHand(cards, HandType.FiveOfAKind, bid);
+        }
+
+        var counts = cards.GroupBy(x => x).Select(g => g.Count()).ToArray();
+
+        if (counts.Any(x => x == 4))
+        {
+            return new CamelHand(cards, HandType.FourOfAKind, bid);
+        }
+
+        if (counts.Any(x => x == 3) && counts.Any(x => x == 2))
+        {
+            return new CamelHand(cards, HandType.FullHouse, bid);
+        }
+
+        if (counts.Any(x => x == 3))
+        {
+            return new CamelHand(cards, HandType.ThreeOfAKind, bid);
+        }
+
+        if (counts.Count(x => x == 2) == 2)
+        {
+            return new CamelHand(cards, HandType.TwoPair, bid);
+        }
+
+        if (counts.Contains(2))
+        {
+            return new CamelHand(cards, HandType.OnePair, bid);
+        }
+
+        return new CamelHand(cards, HandType.HighCard, bid);
+    }
+
+    protected override object InternalPart2()
+    {
+        var hands = Parse();
+        hands.Sort(_part2Comparer);
+
+        var score = hands
+            .Select((hand, i) => hand.Bid * (i + 1))
+            .Sum();
+
+        return score;
+    }
+
+    public static CamelCard CharToCard(char c)
+    {
+        return c switch
+        {
+            '2' => CamelCard.Two,
+            '3' => CamelCard.Three,
+            '4' => CamelCard.Four,
+            '5' => CamelCard.Five,
+            '6' => CamelCard.Six,
+            '7' => CamelCard.Seven,
+            '8' => CamelCard.Eight,
+            '9' => CamelCard.Nine,
+            'T' => CamelCard.T,
+            'J' => CamelCard.J,
+            'Q' => CamelCard.Q,
+            'K' => CamelCard.K,
+            'A' => CamelCard.A,
+            _ => throw new ArgumentException($"Invalid character {c}", nameof(c))
+        };
+    }
+
+    private static readonly HandComparer _part2Comparer = new(true);
+    private static readonly HandComparer _part1Comparer = new();
+
+    public static CamelHand MakeStrongestHand(CamelHand hand)
+    {
+        if (hand.Cards.All(x => x != CamelCard.J))
+        {
+            return hand;
+        }
+
+        var handStr = hand.ToString();
+
+        var potentialHands = Enum.GetValues<CamelCard>()
+            .Select(card => handStr.Replace("J", card.GetStrValue()))
+            .Select(ParseOne)
+            .OrderByDescending(x => x.Type)
+            .ToList();
+
+        var bestType = potentialHands[0].Type;
+
+        potentialHands = potentialHands
+            .Where(y => y.Type == bestType)
+            .ToList();
+
+        potentialHands.Sort(_part2Comparer);
+
+        return potentialHands[0];
+    }
+}
+
+public enum CamelCard
+{
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    T,
+    J,
+    Q,
+    K,
+    A,
+}
+
+public enum HandType
+{
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    FullHouse,
+    FourOfAKind,
+    FiveOfAKind,
+}
+
+public record CamelHand(CamelCard[] Cards, HandType Type, int Bid)
+{
+    public override string ToString()
+        => $"{string.Join("", this.Cards.Select(x => x.GetStrValue()))} {Bid}";
+}
+
+public class HandComparer : Comparer<CamelHand>
+{
+    private readonly bool _isPart2;
+
+    public HandComparer(bool isPart2 = false)
+    {
+        _isPart2 = isPart2;
+    }
+
+    public override int Compare(CamelHand? x, CamelHand? y)
+    {
+        if (x is null || y is null)
+        {
+            throw new ArgumentNullException("x or y", "Can't compare nulls");
+        }
+
+        if (x.Type != y.Type)
+        {
+            if (!_isPart2)
+            {
+                return x.Type.CompareTo(y.Type);
+            }
+
+            var newX = Day07.MakeStrongestHand(x);
+            var newY = Day07.MakeStrongestHand(y);
+
+            if (newX.Type != newY.Type)
+            {
+                return newX.Type.CompareTo(newY.Type);
+            }
+        }
+
+        foreach ((CamelCard first, CamelCard second) in x.Cards.Zip(y.Cards))
+        {
+            if (first == second)
+            {
+                continue;
+            }
+
+            if (_isPart2)
+            {
+                if (first == CamelCard.J)
+                    return -1;
+                if (second == CamelCard.J)
+                    return 1;
+            }
+
+            return first.CompareTo(second);
+        }
+
+        return 0;
+    }
+}
+
+public static class Day07Extensions
+{
+    public static string GetStrValue(this CamelCard card)
+        => card switch
+        {
+            CamelCard.Two => "2",
+            CamelCard.Three => "3",
+            CamelCard.Four => "4",
+            CamelCard.Five => "5",
+            CamelCard.Six => "6",
+            CamelCard.Seven => "7",
+            CamelCard.Eight => "8",
+            CamelCard.Nine => "9",
+            _ => card.ToString()
+        };
 }
--- tests/AdventTests/2023/Day07Tests.cs
+++ tests/AdventTests/2023/Day07Tests.cs
@@ -1,6 +1,63 @@
-﻿namespace AdventTests._2023;
+﻿using AdventOfCode._2023;
 
-public sealed class Day07Tests
+namespace AdventTests._2023;
+
+public class Day07Tests
 {
-    
+    private string _testInput = """
+                                32T3K 765
+                                T55J5 684
+                                KK677 28
+                                KTJJT 220
+                                QQQJA 483
+                                """;
+
+    [Fact]
+    public void Part1()
+    {
+        var day = new Day07();
+
+        day.SetTestInput(_testInput);
+        day.Part1();
+        day.Part1Answer.ShouldBe("6440");
+    }
+
+    [Fact]
+    public void Part2()
+    {
+        var day = new Day07();
+
+        day.SetTestInput(_testInput);
+        day.Part2();
+        day.Part2Answer.ShouldBe("5905");
+    }
+
+    [Fact]
+    public void CanUpgrade()
+    {
+        var hand = Day07.ParseOne("QJJQ2 500");
+        var upgraded = Day07.MakeStrongestHand(hand);
+
+        upgraded.Type.ShouldBe(HandType.FourOfAKind);
+
+        var fiveHand = Day07.ParseOne("JJQQQ 1");
+        var upgFiveHand = Day07.MakeStrongestHand(fiveHand);
+        upgFiveHand.Type.ShouldBe(HandType.FiveOfAKind);
+    }
+
+    [Fact]
+    public void Idk()
+    {
+        var hand1 = Day07.ParseOne("JJQQQ 1");
+        var hand2 = Day07.ParseOne("QQQQQ 2");
+        var hand3 = Day07.ParseOne("JJJJJ 3");
+        var hand4 = Day07.ParseOne("JJJJ2 3");
+
+        var hands = new List<CamelHand> { hand2, hand1, hand3, hand4 };
+
+        hands.Sort(new HandComparer(true));
+
+        hands[0].ToString().ShouldBe("JJQQQ 1");
+        hands[1].ToString().ShouldBe("QQQQQ 2");
+    }
 }
